608f135d89f5582c8e940387ab251a0be2a025cc
mohammedEssam0 <98328382+mohammedEssam0@users.noreply.github.com>
2022-06-01 17:34:25 +0200

    Level2

---
+++ LyceeBalzacApi/Migrations/20220601121425_AddLevel2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LyceeBalzacApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevel2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Level2",
+                columns: table => new
+                {
+                    Level2Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Level2_Name_A = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level2_Name_E = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Entry_ID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Level2", x => x.Level2Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Level2");
+        }
+    }
+}
